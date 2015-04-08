370f7d8a0203456252886c7ec3155a00017580bb
yonniz <yonniz@gmail.com>
2015-04-08 18:17:57 +0300

    another commit

---
+++ public_html/practice.php
@@ -0,0 +1,27 @@
+<?php
+
+//echo "Hello World";
+//declare variables
+
+//$i =1;
+//echo $i;
+
+//string
+
+//$name = "Yonni";
+//echo $name;
+
+//boolean
+
+//$payment_status = TRUE;
+//echo $payment_status;
+
+//mathematical operations
+
+//$i = 1;
+//$j = 2;
+
+//$z = $i + $j;
+//echo $z;
+
+?>
